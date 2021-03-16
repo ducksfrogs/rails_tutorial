df15f2613f64600d0811ebed0a39857d1610809a
ma <ma_yamaki@yahoo.com>
2021-03-16 14:15:35 +0900

    d

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
 #  get 'static_pages/about'
 #  get 'static_pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
 end
